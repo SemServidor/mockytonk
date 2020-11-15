b087b25ed8dffc00374b89354f9aa4f97a0b8bb5
Evandro Pires da Silva <evandro@evandropires.com.br>
2020-11-15 18:16:45 -0300

    Refactoring

--- add_mock.py
+++ add_mock.py
@@ -6,15 +6,35 @@ import json
 def main(event, context):
     body = json.loads(event["body"])
 
-    if "secretKey" not in body:
-        secret_key = str(uuid.uuid4())
-    else:
-        secret_key = body["secretKey"]
+    secret_key = get_secret_key(body)
+    id = new_id()
 
-    id = str(uuid.uuid4())
+    put_item(event, secret_key, id)
+
+    response_body = {
+        "mockContract": {
+            "statusCode": body["statusCode"],
+            "response": body["response"],
+            "headers": body["headers"]
+        },
+        "id": id,
+        "secretKey": secret_key,
+        "url": get_proxy_url(event, id)
+    }
+    response = {
+        "statusCode": 201,
+        "body": json.dumps(response_body)
+    }
+
+    return response
+
+
+def put_item(event, secret_key, id):
+    body = json.loads(event["body"])
 
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     stage = event["requestContext"]["stage"]
+
     table = dynamodb.Table('mockytonk-{}'.format(stage))
     table.put_item(
         Item={
@@ -27,19 +47,21 @@ def main(event, context):
         }
     )
 
-    body = {
-        "mockContract": {
-            "statusCode": body["statusCode"],
-            "response": body["response"],
-            "headers": body["headers"]
-        },
-        "id": id,
-        "secretKey": secret_key,
-        "url": "https://{}/{}/proxy/{}".format(event["requestContext"]["domainName"], stage, id)
-    }
-    response = {
-        "statusCode": 201,
-        "body": json.dumps(body)
-    }
 
-    return response
+def new_id():
+    return str(uuid.uuid4())
+
+
+def get_secret_key(body):
+    if "secretKey" not in body:
+        return str(uuid.uuid4())
+    return body["secretKey"]
+
+
+def get_proxy_url(event, id):
+    app_context = event["requestContext"]["stage"]
+    if app_context == "prod":
+        app_context = ""
+    else:
+        app_context += "/"
+    return "https://{}/{}proxy/{}".format(event["requestContext"]["domainName"], app_context, id)
